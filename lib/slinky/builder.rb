e14d138186ec2313f962c2fde6bc7b79431bc3b1
Micah Wylde <mwylde@wesleyan.edu>
2011-04-14 02:33:41 -0400

    more

--- lib/slinky/builder.rb
+++ lib/slinky/builder.rb
@@ -5,7 +5,12 @@ module Slinky
     end
 
     def build
-      
+      manifest = Manifest.build(@dir)
+      manifest.files.each do |file|
+        File.open(file) do
+          
+        end
+      end
     end
   end
 end
