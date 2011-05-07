7df93b167a6eb4de78f0431977c506b18a7127a4
Hemant Kumar <hkumar@crri.co.in>
2011-05-07 12:37:56 +0530

    add Separate sass gem as depenency and install signal handler for interrupt

--- lib/slinky/runner.rb
+++ lib/slinky/runner.rb
@@ -1,6 +1,7 @@
 module Slinky
   class Runner
     class << self
+      Signal.trap('INT') { puts "Slinky fading away ... "; exit(0); }
       def run
         EM::run {
           EM::start_server "0.0.0.0", 5323, Slinky::Server
