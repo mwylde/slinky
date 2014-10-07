3fc568af45317913745bce449cfc94b9dcc97ee8
Alex Lopatin <alex@siftscience.com>
2014-10-07 10:41:06 -0700

    Declaring variables outside of a for loop

--- ext/transitive_closure/transitive_closure.c
+++ ext/transitive_closure/transitive_closure.c
@@ -7,25 +7,23 @@ static VALUE all_paths_costs(VALUE module, VALUE rsize, VALUE distArray)
 
     // Create copy of distArray
     int cDistArray[distArrayLen];
-    for (int i = 0; i < distArrayLen; i++) {
+    int i, k, j;
+    for (i = 0; i < distArrayLen; i++) {
         cDistArray[i] = FIX2INT(rb_ary_entry(distArray, i));
     }
 
-    for (int k = 0; k < size; k++) {
-        for (int i = 0; i < size; i++) {
-            for (int j = 0; j < size; j++) {
+    for (k = 0; k < size; k++) {
+        for (i = 0; i < size; i++) {
+            for (j = 0; j < size; j++) {
                 if (cDistArray[size*i+j] > cDistArray[size*i+k] + cDistArray[size*k+j]) {
                     cDistArray[size*i+j] = cDistArray[size*i+k] + cDistArray[size*k+j];
                 }
             }
         }
     }
-            // if dist[size*i+j] > dist[size*i+k] + dist[size*k+j]
-            //   dist[size*i+j] = dist[size*i+k] + dist[size*k+j]
-            // end
 
     // Copy cDistArray back into distArray
-    for (int i = 0; i < distArrayLen; i++) {
+    for (i = 0; i < distArrayLen; i++) {
         rb_ary_store(distArray, i, INT2FIX(cDistArray[i]));
     }
 
